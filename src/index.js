11937d76a65cf3079223812456259b4a227b0261
Andrey Uvarov <andrey-1@ukr.net>
2022-11-15 20:42:42 +0200

    exercise11-ver2-2

--- src/index.js
+++ src/index.js
@@ -1,91 +1,90 @@
 import './sass/index.scss';
-
+import someTemplate from './templates/template.hbs';
 import { axiosSearchImages } from './axiosSearchImages';
-import { doRendering } from './doRendering';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
+
+const infiniteScroll = require('infinite-scroll');
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const axios = require('axios').default;
 const formEl = document.querySelector('.search-form');
 const submitFormEl = document.querySelector('.submit-form');
-const buttonLoadMore = document.querySelector('.load-more');
+// const buttonLoadMore = document.querySelector('.load-more');
 const galaryEl = document.querySelector('.gallery');
 
-let counter = 1;
 let nameInput;
+// console.log(infiniteScroll);
+// infScroll.on('error', function () {
+//   Notiflix.Notify.failure(
+//     'Sorry, there are no images matching your search query. Please try again.'
+//   );
+// });
 
-buttonLoadMore.style.display = 'none';
-
-// плавный скролл
-function toScroll() {
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
-
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-    //
-  });
+function sendNegativeMessage() {
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
 }
-// плавный скролл
+function sendPositiveMessage(date) {
+  Notiflix.Notify.success(`Hooray! We found ${date} images.`);
+}
+
+// поиск сабмит
+async function asyncRequestSubmit(value, page) {
+  try {
+    const promise = await axiosSearchImages(value, page);
+    const data = await promise.data;
+    if (data.hits.length == 0) {
+      sendNegativeMessage();
+      // buttonLoadMore.style.display = 'none';
+      submitFormEl.textContent = 'Searh';
+      galaryEl.innerHTML = '';
+      return;
+    }
+
+    sendPositiveMessage(data.totalHits);
+    const Array = await data.hits;
+    galaryEl.innerHTML = someTemplate(Array);
+    toLightBox();
+    submitFormEl.textContent = 'Searh';
+  } catch (error) {
+    Notiflix.Notify.failure(`${error}`);
+  }
+}
+// // поиск сабмит
 
 formEl.addEventListener('submit', onClickForm);
 
 function onClickForm(event) {
-  buttonLoadMore.style.display = 'none';
+  // buttonLoadMore.style.display = 'none';
   submitFormEl.textContent = 'Searh...';
   event.preventDefault();
   const inputEl = formEl.elements.searchQuery;
   nameInput = inputEl.value;
+  asyncRequestSubmit(nameInput, 1);
 
-  axiosSearchImages(inputEl.value, '1')
-    .then(promise => promise.data)
-    .then(data => {
-      if (data.hits.length == 0) {
-        buttonLoadMore.style.display = 'none';
-        submitFormEl.textContent = 'Searh';
-        galaryEl.innerHTML = '';
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
+  let infScroll = new infiniteScroll('.gallery', {
+    path: function () {
+      {
+        if (this.pageIndex == 1) {
+          this.pageIndex = 2;
+        }
       }
-      console.log(data);
-      buttonLoadMore.style.display = 'block';
+      // console.log(this.pageIndex);
+      // console.log(this.loadCount);
+      return `https://pixabay.com/api/?key=31270894-f030383025237e7d53b6441a5&q=${nameInput}&page=${this.pageIndex}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`;
+    },
+    responseBody: 'json',
+    status: '.page-load-status',
+    history: false,
+  });
 
-      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-      return data.hits;
-    })
-    .then(Array => {
-      galaryEl.innerHTML = doRendering(Array);
-      toLightBox();
-      submitFormEl.textContent = 'Searh';
-    });
-}
-buttonLoadMore.addEventListener('click', () => onClickLoadMore(nameInput));
-
-function onClickLoadMore(value) {
-  counter += 1;
-  axiosSearchImages(value, counter)
-    .then(promise => promise.data)
-    .then(data => {
-      if (data.hits.length == 0) {
-        buttonLoadMore.style.display = 'none';
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
-      }
-      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-      return data.hits;
-    })
-    .then(Array => {
-      galaryEl.insertAdjacentHTML('beforeend', doRendering(Array));
-      toScroll();
-      toLightBox();
-    });
+  infScroll.on('load', function (array) {
+    galaryEl.insertAdjacentHTML('beforeend', someTemplate(array.hits));
+    toLightBox();
+  });
 }
 
 // Эффект Lightbox
